f46eb4c729197e3040035e30b5410eb0069b2c6b
JonasLangeVonStocmeier <vonstocmeier@gmail.com>
2024-10-14 12:49:23 +0200

    Added folder structure and first ViewModels

--- WpfTasksListMVVM/ViewModels/MainWindowViewModel.cs
+++ WpfTasksListMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfTasksListMVVM.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
+        // going to update when using observable collections 
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
